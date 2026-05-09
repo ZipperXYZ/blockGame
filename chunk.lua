f9c30886bb1a5be1c3aa48e217d50a21e2a0bf57
antgamingx <antgamingx123@gmail.com>
2026-05-09 15:15:03 -0400

    fini le skelete de chunk

--- chunk.lua
+++ chunk.lua
@@ -10,6 +10,7 @@ function Chunk:init(chunkX,chunkY,chunkSize)
     self.ChunkX=chunkX
     self.chunkY=chunkY
     self.chunkSize=chunkSize
+    self.generationStatus="none"
     self.tiles={}
     self.backTiles={}
     self.topTiles={}
@@ -31,6 +32,9 @@ function Chunk:init(chunkX,chunkY,chunkSize)
     end
 end
 
+--la pluspart des fonctions ici ne sont pas sensé être utilisé en tant que tels, à par la génération, ils devraient juste être utilisé
+--par un objet world
+
 
 --getTile(xinchunk,yinchunk,layer) --retourne un objet tile selon le nom de la tile
     --vue que lua est bien, on pourrait metre comme layer 'light' pis ça donnerait le tableau lumière ({1,1,1,1}) d'une tile au lieu d'un objet tile
@@ -41,22 +45,61 @@ end
     --layer peut etre soit tile, back et top, usually faudrait checker si une tile peut être placé en top (comme gazon ou les ores)
     --la pluspart des tiles peuvent être placé dans tile, mais seulement certaines peuvent être placé dans back (peut être faire une
     --fonction canBeWall dans tileDef)
-function Chunk:placeTile(xinchunk,yinchunk,layer,force)
+function Chunk:placeTile(tile,xinchunk,yinchunk,layer,force)
 
 end
---la pluspart des fonctions ici ne sont pas sensé être utilisé en tant que tels, à par la génération, ils devraient juste être utilisé
---par un objet world
+
+--getGenerationStatus() -- return world gen status of the chunk
+function Chunk:getGenerationStatus()
+    return self.generationStatus
+end
+
+--generate(step,chunkX,chunkY,worldSeed,depthProgression,biomeSize,biomeList) -- generate according to step
+function Chunk:generate(step,stepList,worldSeed,depthProgression,biomeSize,biomeList)
+
+end
+
+--advanceGenerationStatus() --comme un setGenerationStatus, mais change vers le prochain, mis à la fin d'une étape de generate
+function Chunk:advanceGenerationStatus(stepList)
+
+end
+
+--getTerrain(worldPosX,wordPosY,worldSeed,depthProgression,biomeSize,biomeList) -- retourne soit 'air', ou 'dirt' (peut être false or true?)
+    --est appliqué à la base base de la génération pour déterminer si il y a de l'air ou du terrain à un endroit
+function Chunk:getTerrain(worldPosX,wordPosY,worldSeed,depthProgression,biomeSize,biomeList)
+
+end
+
+--convertChunkPosToWorldPos(posInChunkX,posInChunkY) --return worldPosX,worldPosY
+function Chunk:convertChunkPosToWorldPos(posInChunkX,posInChunkY)
+
+end
+
+--getBiome(x,y) --return le nom du biome
+function Chunk:getBiome(posInChunkX,posInChunkY,worldSeed,depthProgression,biomeSize,biomeList)
+
+end
+
+
+--? getBiome , voir world.lua
+
 
 --new(chunkX,chunkY) --retourne un chunk vide
     --dans chunk il y a un tableau 2d tiles, un tableau 2d back, top, light et properties
     --tiles,back et top comprennent seulement les noms des tiles, pas leur objet en tant que tel
+--[[
 
---getGenerationStatus() -- return world gen status of the chunk
---advanceGenerationStatus() --comme un setGenerationStatus, mais change vers le prochain, mis à la fin d'une étape de generate
---generate(step,chunkX,chunkY,worldSeed,depthProgression,biomeSize,biomeList) -- generate according to step
---getTerrain(worldPosX,wordPosY,worldSeed,depthProgression,biomeSize,biomeList) -- retourne soit 'air', ou 'dirt' (peut être false or true?)
-    --est appliqué à la base base de la génération pour déterminer si il y a de l'air ou du terrain à un endroit
+--ceux si vont être dans world à la place
 --updateLight(neighboringChunks) -- (getNeighboringChunks())
+function Chunk:updateLight()
+
+end
+
 --getClosestNonSolidTile(worldPosX,wordPosY) --utilisé dans updateLight
---convertChunkPosToWorldPos(posInChunkX,posInChunkY) --return worldPosX,worldPosY
---? getBiome , voir world.lua
+function Chunk:getClosestNonSolidTile(worldPosX,wordPosY)
+
+end
+
+]]
+
+

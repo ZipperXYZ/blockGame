e0dd8e003fad89861d04c5f7667a84a0d6f16a17
NotaRealBunnyz <notarealbunnyz@gmail.com>
2026-05-09 14:37:59 -0400

    TileDef fini ?

--- tiledef.lua
+++ tiledef.lua
@@ -5,12 +5,14 @@ require "class/superClass"
 Tile = SuperClass:extend()
 Tile.className = "Tile"
 
+--init()
 function Tile:init(tilename, tiletype, textureName, quadName, flags)
     self.name = tilename or "none"
     self.type = tiletype or "empty"
     self.textureName = textureName or "none"
     self.quadName = quadName or "none"
-    self.flags = flags or nil
+    self.flags = flags or {}
+    self.properties = {}
 
     --va regarde la liste globale des texture si elle comprend le name de la texture pis si non elle va créer la texture
     if not textures["textures"][textureName] and textureName ~= "none" then
@@ -18,13 +20,13 @@ function Tile:init(tilename, tiletype, textureName, quadName, flags)
     end
 
     --va regarde la liste globale des quads si elle comprend le name de la quad pis si non elle va créer la quad selon des paramètres
-    if not textures["quads"][quadName] and quadName ~= "none" then
-        textures["quads"][quadName] = love.graphics.newQuad(
-            flags["newQuad"][1] * flags["newQuad"][5]
-            , flags["newQuad"][2] * flags["newQuad"][5]
-            , flags["newQuad"][3] * flags["newQuad"][5]
-            , flags["newQuad"][4] * flags["newQuad"][5]
-            , textures["textures"][textureName])
+    if not textures["quads"][self.quadName] and self.quadName ~= "none" then
+        textures["quads"][self.quadName] = love.graphics.newQuad(
+            self.flags["newQuad"][1] * self.flags["newQuad"][5]
+            , self.flags["newQuad"][2] * self.flags["newQuad"][5]
+            , self.flags["newQuad"][3] * self.flags["newQuad"][5]
+            , self.flags["newQuad"][4] * self.flags["newQuad"][5]
+            , textures["textures"][self.textureName])
     end
 
     --flags comprend tout le reste, la pluspart vont être nil, donc assigner des variables pour tout de base
@@ -33,14 +35,35 @@ function Tile:init(tilename, tiletype, textureName, quadName, flags)
     self.canBeMined = flags.canBeMined or true
 end
 
+--getName()
+function Tile:getName()
+    return self.name
+end
+
+--getTexture() (ne retourne pas le nom de la texture mais le fichier texture, meme si il est pas storé dedans, il le cherche par son nom)
+function Tile:getTexture()
+    return textures["textures"][self.textureName]
+end
+
+--getQuad() (ne retourne pas le nom du quad mais le quad en tant que tel, meme si il est pas storé dedans, il le cherche par son nom)
+function Tile:getQuad()
+    return textures["quads"][self.quadName]
+end
+
+--get... (pour tout les propriétés)
+function Tile:getType()
+    return self.type
+end
+
+function Tile:getFlags()
+    return self.flags
+end
+
 --  il s'agit ici de la défénition d'une tuile, est la même pour chaque tiles du même type dans le monde
 --  pour les propriété unique à une tuile (inventaire d'un coffre, orientation, vie de la tuile), utilise
 --  getTilePropreties et setTileProprety dans l'objet monde
---new()
---getName()
---getTexture() (ne retourne pas le nom de la texture mais le fichier texture, meme si il est pas storé dedans, il le cherche par son nom)
---getQuad() (ne retourne pas le nom du quad mais le quad en tant que tel, meme si il est pas storé dedans, il le cherche par son nom)
---get... (pour tout les propriétés)
+
 --il n'y a pas de setters vue qu'il s'agie seulement de la défénition des tuiles et ne devrait jamais changer
+
 --? place(), fonction qui se passe quand tu placeras une tile, unique à certain type de tiles, à vérifier
 --? remove()

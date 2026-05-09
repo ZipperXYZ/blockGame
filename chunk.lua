8e83a3e21fcd1577a6988f6296850ea2a3cc2bb8
antgamingx <antgamingx123@gmail.com>
2026-05-09 14:40:03 -0400

    chunks suite

--- chunk.lua
+++ chunk.lua
@@ -15,13 +15,13 @@ function Chunk:init(chunkX,chunkY,chunkSize)
     self.topTiles={}
     self.lights={}
     self.tileProperties={}
-    for local ix=1, self.chunkSize do
+    for ix=1, self.chunkSize do
         self.tiles[ix]={}
         self.backTiles[ix]={}
         self.topTiles[ix]={}
         self.lights[ix]={}
         self.tileProperties[ix]={}
-        for local iy=1, self.chunkSize do
+        for iy=1, self.chunkSize do
             self.tiles[ix][iy]="none"
             self.backTiles[ix][iy]="none"
             self.topTiles[ix][iy]="none"
@@ -31,18 +31,26 @@ function Chunk:init(chunkX,chunkY,chunkSize)
     end
 end
 
+
+--getTile(xinchunk,yinchunk,layer) --retourne un objet tile selon le nom de la tile
+    --vue que lua est bien, on pourrait metre comme layer 'light' pis ça donnerait le tableau lumière ({1,1,1,1}) d'une tile au lieu d'un objet tile
+function Chunk:getTile(xinchunk,yinchunk,layer,force)
+
+end
+--placeTile(xinchunk,yinchunk,layer,bool:force) 
+    --layer peut etre soit tile, back et top, usually faudrait checker si une tile peut être placé en top (comme gazon ou les ores)
+    --la pluspart des tiles peuvent être placé dans tile, mais seulement certaines peuvent être placé dans back (peut être faire une
+    --fonction canBeWall dans tileDef)
+function Chunk:placeTile(xinchunk,yinchunk,layer,force)
+
+end
 --la pluspart des fonctions ici ne sont pas sensé être utilisé en tant que tels, à par la génération, ils devraient juste être utilisé
 --par un objet world
 
 --new(chunkX,chunkY) --retourne un chunk vide
     --dans chunk il y a un tableau 2d tiles, un tableau 2d back, top, light et properties
     --tiles,back et top comprennent seulement les noms des tiles, pas leur objet en tant que tel
---placeTile(xinchunk,yinchunk,layer,bool:force) 
-    --layer peut etre soit tile, back et top, usually faudrait checker si une tile peut être placé en top (comme gazon ou les ores)
-    --la pluspart des tiles peuvent être placé dans tile, mais seulement certaines peuvent être placé dans back (peut être faire une
-    --fonction canBeWall dans tileDef)
---getTile(xinchunk,yinchunk,layer) --retourne un objet tile selon le nom de la tile
-    --vue que lua est bien, on pourrait metre comme layer 'light' pis ça donnerait le tableau lumière ({1,1,1,1}) d'une tile au lieu d'un objet tile
+
 --getGenerationStatus() -- return world gen status of the chunk
 --advanceGenerationStatus() --comme un setGenerationStatus, mais change vers le prochain, mis à la fin d'une étape de generate
 --generate(step,chunkX,chunkY,worldSeed,depthProgression,biomeSize,biomeList) -- generate according to step

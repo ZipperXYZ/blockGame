e5a4de1881d813bd36c98d2e145ed97631fbe5db
antgamingx <antgamingx123@gmail.com>
2026-05-22 23:04:58 -0400

    1 truc

--- items/item.lua
+++ items/item.lua
@@ -7,6 +7,9 @@ function Item:init(itemName,sprite,flags)
     self.itemName = itemName
     self.flags = flags or {}
     self.sprite = sprite or "none"
+
+    self.placeBlock = self.flags.placeBlock or "none"
+    self.canBeUsed = self.flags.canBeUsed or (self.placeBlock~="none")
     --self.textures = textures or {["groundDisplay"]="none",[""]}
 
 end
